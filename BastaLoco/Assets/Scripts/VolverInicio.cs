397b5765ec32bed56b19e44266536bff610e83bb
GuadalupeHolsman <103451377+GuadalupeHolsman@users.noreply.github.com>
2025-07-08 21:34:44 -0300

    no pude con los cables lpmmmm

---
+++ BastaLoco/Assets/Scripts/VolverInicio.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VolverInicio : MonoBehaviour
+{
+    public void IrAInicio()
+    {
+        SceneManager.LoadScene("Inicio");
+    }
+}
